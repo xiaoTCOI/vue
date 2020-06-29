544bbd38d1178f2f10cf8b278732ef8408ea6550
xiaoTCOI <33998534+xiaoTCOI@users.noreply.github.com>
2020-06-29 22:07:32 +0800

    托管项目

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//在main.js中引入js和css(Element-ui)
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import Axios from 'axios'
+
+//让vue引用Element ui
+Vue.use(ElementUI);
+Vue.prototype.$axios = Axios;
 Vue.config.productionTip = false
 
 new Vue({
