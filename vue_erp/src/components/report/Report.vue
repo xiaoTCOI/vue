cb5408492af233c0d7c9fd59aa3217eac3c8bb94
xiaoTCOI <33998534+xiaoTCOI@users.noreply.github.com>
2020-07-17 20:45:15 +0800

    修改了数据统计功能版块的折线图bug

--- vue_erp/src/components/report/Report.vue
+++ vue_erp/src/components/report/Report.vue
@@ -21,6 +21,8 @@ import _ from 'lodash'
 var echarts = require('echarts/lib/echarts');
 // 引入柱状图
 require('echarts/lib/chart/bar');
+// 引入折线图
+require('echarts/lib/chart/line');
 // 引入提示框和标题组件
 require('echarts/lib/component/tooltip');
 require('echarts/lib/component/title');
